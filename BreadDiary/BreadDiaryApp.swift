5c40a110a21a0785d1a053362c590b183969f87b
Michel <mgoni@mlean.com>
2024-11-29 19:29:12 +0100

    Initial Commit

---
+++ BreadDiary/BreadDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreadDiaryApp.swift
+//  BreadDiary
+//
+//  Created by Michel Goñi on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BreadDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
