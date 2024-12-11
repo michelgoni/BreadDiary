510d1158e5aa77799c2446b06fdff7528b07723c
Michel <michelgoni2@gmail.com>
2024-12-11 20:24:32 +0100

    Today is selected by default

--- BreadDiary/Root/Calendar/CalendarFeature.swift
+++ BreadDiary/Root/Calendar/CalendarFeature.swift
@@ -5,7 +5,7 @@ import Foundation
 struct CalendarFeature {
     @ObservableState
     struct State: Equatable {
-        var selectedDate = Date()
+        var selectedDate: Date = Date() // Default to the current date
         var recipeDates: Set<DateComponents>
         var recipesByDate: [Date: String]
         
@@ -14,9 +14,9 @@ struct CalendarFeature {
             let today = Date()
             
             // Create three specific dates
-            let date1 = calendar.date(byAdding: .day, value: -2, to: today)!
-            let date2 = calendar.date(byAdding: .day, value: -5, to: today)!
-            let date3 = calendar.date(byAdding: .day, value: -7, to: today)!
+            let date1 = calendar.date(byAdding: .day, value: -2, to: today)! // 2 days ago
+            let date2 = calendar.date(byAdding: .day, value: -5, to: today)! // 5 days ago
+            let date3 = calendar.date(byAdding: .day, value: -7, to: today)! // 7 days ago
             
             let dates = [date1, date2, date3]
             let dateComponents = Set(dates.map { calendar.dateComponents([.year, .month, .day], from: $0) })
@@ -27,7 +27,6 @@ struct CalendarFeature {
             recipesByDate[date3] = "Whole Wheat Bread"
             
             return State(
-                selectedDate: today,
                 recipeDates: dateComponents,
                 recipesByDate: recipesByDate
             )
