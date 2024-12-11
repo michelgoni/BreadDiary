564305bae713169a98842564fab107640971e2eb
Michel <michelgoni2@gmail.com>
2024-12-11 20:17:06 +0100

    Removed withViewStore

--- BreadDiary/Root/Calendar/CalendarView.swift
+++ BreadDiary/Root/Calendar/CalendarView.swift
@@ -5,45 +5,43 @@ struct CalendarView: View {
     let store: StoreOf<CalendarFeature>
     
     var body: some View {
-        WithViewStore(store, observe: { $0 }) { viewStore in
-            VStack {
-                Text("Calendar").font(.largeTitle)
-                DatePicker(
-                    "",
-                    selection: viewStore.binding(
-                        get: \.selectedDate,
-                        send: { .dateSelected($0) }
-                    ),
-                    in: ...Date(),
-                    displayedComponents: [.date]
-                )
-                .datePickerStyle(.graphical)
-                .tint(.black)
-                .frame(maxWidth: .infinity, maxHeight: 400)
+        VStack {
+            Text("Calendar").font(.largeTitle)
+            DatePicker(
+                "",
+                selection: Binding(
+                    get: { store.selectedDate },
+                    set: { store.send(.dateSelected($0)) }
+                ),
+                in: ...Date(),
+                displayedComponents: [.date]
+            )
+            .datePickerStyle(.graphical)
+            .tint(.black)
+            .frame(maxWidth: .infinity, maxHeight: 400)
+            
+            Divider()
+                .background(.black)
+                .frame(height: 2)
+            
+            VStack(alignment: .leading) {
+                let calendar = Calendar.current
+                let selectedComponents = calendar.dateComponents([.year, .month, .day], from: store.selectedDate)
                 
-                Divider()
-                    .background(.black)
-                    .frame(height: 2)
-                
-                VStack(alignment: .leading) {
-                    let calendar = Calendar.current
-                    let selectedComponents = calendar.dateComponents([.year, .month, .day], from: viewStore.selectedDate)
-                    
-                    if let recipe = viewStore.recipesByDate.first(where: { calendar.dateComponents([.year, .month, .day], from: $0.key) == selectedComponents })?.value {
-                        Text("• \(recipe)")
-                            .padding(.vertical, 4)
-                    } else {
-                        Text("No recipe for this date")
-                            .padding(.vertical, 4)
-                            .foregroundColor(.gray)
-                    }
+                if let recipe = store.recipesByDate.first(where: { calendar.dateComponents([.year, .month, .day], from: $0.key) == selectedComponents })?.value {
+                    Text("• \(recipe)")
+                        .padding(.vertical, 4)
+                } else {
+                    Text("No recipe for this date")
+                        .padding(.vertical, 4)
+                        .foregroundColor(.gray)
                 }
-                .padding()
-                .background(Color.white)
-                .frame(maxWidth: .infinity, alignment: .leading)
             }
             .padding()
+            .background(Color.white)
+            .frame(maxWidth: .infinity, alignment: .leading)
         }
+        .padding()
     }
 }
 
